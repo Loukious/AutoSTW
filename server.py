5bf142f04aabdea38eaa15c1ccc48b9d3fde634e
Loukios <07wael@gmail.com>
2021-06-10 19:04:28 +0100

    first

--- server.py
+++ server.py
@@ -24,7 +24,7 @@ async def webhook(msg):
         await r.post(url, json=data, timeout=10)
 
 
-@aiocron.crontab('15 18 * * *')
+@aiocron.crontab('7 18 * * *')
 async def attime():
     await webhook("@here claiming STW rewards started..")
     print("Started claiming rewards..")
